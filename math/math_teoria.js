5067b3b9bcad01e55167fab0f71d0f249476cf22
Igor <151391534+i120ms@users.noreply.github.com>
2025-09-24 13:44:09 +0200

    Create math_teoria.js

---
+++ math/math_teoria.js
@@ -0,0 +1,38 @@
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E
+
+Math.round(4.6); // returns the nearest integer
+
+Math.ceil(4.9); // returns the value of x rounded up to its nearest integer
+Math.ceil(4.7);
+Math.ceil(4.4);
+Math.ceil(4.2);
+Math.ceil(-4.2);
+
+Math.floor(4.9); // returns the value of x rounded down to its nearest integer
+Math.floor(4.7);
+Math.floor(4.4);
+Math.floor(4.2);
+Math.floor(-4.2);
+
+Math.trunc(4.9); // returns the integer part of x
+Math.trunc(4.7);
+Math.trunc(4.4);
+Math.trunc(4.2);
+Math.trunc(-4.2);
+
+/* Math.sign(x) returns if x is negative, null or positive.
+
+If x is positive it returns 1
+If x is negative it returns -1
+If x is zero, it returns 0
+*/
+Math.sign(-4);
+Math.sign(0);
+Math.sign(4);
