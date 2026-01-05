bee1b42fbb31786c2543d5cef1a1d5d9648ea9ad
Igor <151391534+i120ms@users.noreply.github.com>
2026-01-05 10:41:12 +0100

    Update skrypty.js

--- zadania/wezly_05_01_26/skrypty.js
+++ zadania/wezly_05_01_26/skrypty.js
@@ -1,4 +1,10 @@
-function pokaz(plik) {
-    
-    
+function pokaz(src) {
+    const kontener = document.getElementById("kontener");
+    const img = document.createElement("img");
+
+    img.src = src;
+    img.classList.add("duze-zdjecie");
+
+    kontener.appendChild(img);
+    img.scrollIntoView({ behavior: "smooth" });
 }
